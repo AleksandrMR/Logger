86e5c98a3202b2f4ded4e3d6fd5c7fa7001f6fff
Aleksandr Milashevski <aleksandr.milashevski.ios@gmail.com>
2021-09-09 12:04:23 +0200

    added Logger class

--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -1,3 +1,38 @@
-struct Logger {
-    var text = "Hello, World!"
+import Foundation
+
+class Logger {
+    
+    func debugPrint(
+        _ message: Any,
+        extra1: String = #file,
+        extra2: String = #function,
+        extra3: Int = #line,
+        remoteLog: Bool = false,
+        plain: Bool = false
+    ) {
+        if plain {
+            print(message)
+        }
+        else {
+            let filename = (extra1 as NSString).lastPathComponent
+            print(message, "[\(filename) \(extra3) line]")
+        }
+        
+        // if remoteLog is true record the log in server
+        if remoteLog {
+            //            if let msg = message as? String {
+            //                logEvent(msg, event: .error, param: nil)
+            //            }
+        }
+    }
+    
+    /// pretty print
+    func prettyPrint(_ message: Any) {
+        dump(message)
+    }
+    
+    func printDocumentsDirectory() {
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        print("Document Path: \(documentsPath)")
+    }
 }
