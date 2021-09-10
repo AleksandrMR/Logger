70f14f90921db29284b4c3faa7413de358d2185a
Aleksandr Milashevski <aleksandr.milashevski.ios@gmail.com>
2021-09-10 12:55:43 +0200

    updateVersion_1.0.7

--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -6,55 +6,19 @@ public class Logger {
     
     public init(){}
     
-    public func debugPrint(
-        _ message: Any,
-        extra1: String = #file,
-        extra2: String = #function,
-        extra3: Int = #line,
-        remoteLog: Bool = false,
-        plain: Bool = false
-    ) {
-        if plain {
-            print(message)
-        }
-        else {
-            let filename = (extra1 as NSString).lastPathComponent
-            print("[⚠️]", message, "[\(filename) \(extra3) line]")
-        }
+    public func debugPrint(_ message: Any,
+                           extra1: String = #file,
+                           extra2: String = #function,
+                           extra3: Int = #line) {
         
-        // if remoteLog is true record the log in server
-        if remoteLog {
-            if let msg = message as? String {
-                //                logEvent(msg, event: .error, param: nil)
-            }
-        }
+        let filename = (extra1 as NSString).lastPathComponent
+        print("[⚠️]", "[\(filename) \(extra3) line]", message)
     }
     
     /// pretty print
-    public func prettyPrint(_ message: Any?) -> String {
-        if let jsonDict = message as? [String: Any] {
-            if let jsonData = try? JSONSerialization.data(
-                withJSONObject: jsonDict,
-                options: [.prettyPrinted]) {
-                if let jsonText = String(data: jsonData,
-                                         encoding: .utf8) {
-                    return jsonText
-                }
-            }
-        }
-        
-        if let jsonArray = message as? [[String: Any]] {
-            if let jsonData = try? JSONSerialization.data(
-                withJSONObject: jsonArray,
-                options: [.prettyPrinted]) {
-                if let jsonText = String(data: jsonData,
-                                         encoding: .utf8) {
-                    return jsonText
-                }
-            }
-        }
-        
-        return "Not a JSON"
+    public func prettyPrint(_ message: AnyCollection<Any>) {
+        debugPrint(message)
+        print("\(message.json())\n")
     }
     
     public func dumpPrint(_ message: Any) {
@@ -64,5 +28,20 @@ public class Logger {
     public func printDocumentsDirectory() {
         let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
         print("Document Path: \(documentsPath)")
+    }
+}
+
+public extension Collection {
+    
+    /// Convert self to JSON String.
+    /// Returns: the pretty printed JSON string or an empty string if any error occur.
+    func json() -> String {
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: self, options: [.prettyPrinted])
+            return String(data: jsonData, encoding: .utf8) ?? "{}"
+        } catch {
+            print("json serialization error: \(error)")
+            return "{}"
+        }
     }
 }
